f80ce3517af6999e2fc24dc3ff168cba1792be0c
liangzuobin <liangzuobin@liangzuobin-meican.local>
2016-12-21 16:26:12 +0800

    file hierarchy

---
+++ filehierarchy/my/inaccessible.rs
@@ -0,0 +1,4 @@
+#[allow(dead_code)]
+pub fn public_funciton() {
+    println!("call `my::inaccessible::public_function`");
+}
---
+++ filehierarchy/split.rs
@@ -0,0 +1,15 @@
+mod my;
+
+fn function() {
+    println!("call `function`");
+}
+
+fn main() {
+    my::function();
+
+    function();
+
+    my::indirect_access();
+
+    my::nested::function();
+}
