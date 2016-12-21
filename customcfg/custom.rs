3dd6627c14ed233cc076a6f32172490542bb187f
liangzuobin <liangzuobin@liangzuobin-meican.local>
2016-12-21 16:55:02 +0800

    cfg

---
+++ customcfg/custom.rs
@@ -0,0 +1,10 @@
+#[cfg(some_condition)]
+fn conditional_function() {
+    println!("condition met!")
+}
+
+fn main() {
+    conditional_function();
+}
+
+// `rustc --cfg some_condition cust.rs && ./custom`
